32a514741821e0695b18f5f4808e7236b91beb87
onkark98 <onkark98@gmail.com>
2024-03-02 21:56:49 +0530

    Rest Template used for communication b/w 2 MS

--- movie-catalogue-service/src/main/java/io/javabrains/moviecatalogueservice/MovieCatalogueServiceApplication.java
+++ movie-catalogue-service/src/main/java/io/javabrains/moviecatalogueservice/MovieCatalogueServiceApplication.java
@@ -2,9 +2,16 @@ package io.javabrains.moviecatalogueservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class MovieCatalogueServiceApplication {
+	@Bean
+	public RestTemplate restTemplate()
+	{
+		return new RestTemplate();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MovieCatalogueServiceApplication.class, args);
--- movie-catalogue-service/src/main/java/io/javabrains/moviecatalogueservice/resources/MovieCatalogueResource.java
+++ movie-catalogue-service/src/main/java/io/javabrains/moviecatalogueservice/resources/MovieCatalogueResource.java
@@ -1,22 +1,48 @@
 package io.javabrains.moviecatalogueservice.resources;
 
 import io.javabrains.moviecatalogueservice.models.CatalogueItem;
+import io.javabrains.moviecatalogueservice.models.Movie;
+import io.javabrains.moviecatalogueservice.models.Rating;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import static java.util.Collections.singletonList;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogueResource {
 
+    @Autowired
+    private RestTemplate restTemplate;
     @RequestMapping("/{userId}")
     public List<CatalogueItem> getCatalogue(@PathVariable("userId") String userId)
     {
-        return singletonList(
-                new CatalogueItem("Transformers", "Test", 4)
+        // Get all movie IDs which are rated;
+        // For each movie ID get the details from the movie-info-service
+        //Put them all together
+
+        List<Rating> ratings = Arrays.asList(
+                new Rating("1234", 4),
+                new Rating("5678", 3)
         );
+
+        return ratings.stream().map(rating -> {
+                    //In the below code we are unmarshalling the string response to a movie object using ".getForObject()"
+                    Movie movie = restTemplate.getForObject("http://localhost:8082/movies/"+rating.getMovieId(), Movie.class);
+                    return new CatalogueItem(movie.getName(), "Test", rating.getRating());
+                })
+                .collect(Collectors.toList());
+
+/*        return singletonList(
+                new CatalogueItem("Transformers", "Test", 4)
+        );*/
     }
 }
